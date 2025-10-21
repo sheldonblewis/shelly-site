1c506bd8612111c3c345eb4f8c856dc073d767bb
Sheldon Lewis <sheldonblewis@gmail.com>
2025-10-21 17:24:41 -0400

    temp site initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif',
+    }}>
+      <h1>sheldon lewis</h1>
+      <p>builder, designer, and storyteller.</p>
+    </main>
+  )
+}
