2f5b5b6edd9c3ecad12dd893db1bb76b0c4e2478
Sheldon Lewis <sheldonblewis@gmail.com>
2025-11-21 14:14:27 -0500

    :)

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       fontFamily: 'sans-serif',
     }}>
       <h1>sheldon lewis</h1>
-      <p>solving the world's hardest problems with the most interesting people.</p>
+      <p>{"solving the world's hardest problems with the most interesting people :)"}</p>
     </main>
   )
 }
