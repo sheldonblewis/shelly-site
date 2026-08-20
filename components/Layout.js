92353753afba83b70fceae31d84fd0dac201ae75
Sheldon Lewis <sheldonblewis@gmail.com>
2026-08-19 23:53:30 -0700

    Reframe portrait and circular favicon

--- components/Layout.js
+++ components/Layout.js
@@ -7,8 +7,8 @@ export default function Layout({ children }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/headshot-square.png" type="image/png" sizes="512x512" />
-        <link rel="apple-touch-icon" href="/headshot-square.png" />
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="512x512" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
       </Head>
       <div className="page-container">
         <Nav />
