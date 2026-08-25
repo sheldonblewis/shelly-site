71b67ca3ec9e7e5014b683e9cd6ccdf90e8784cc
Sheldon Lewis <sheldonblewis@gmail.com>
2026-08-25 16:36:47 -0400

    Redirect legacy state machines route

--- pages/state-machines.js
+++ pages/state-machines.js
@@ -1,16 +1,12 @@
-import PageMeta from '@/components/PageMeta'
-import BackLink from '@/components/BackLink'
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/thoughts/state-machines',
+      permanent: true,
+    },
+  }
+}
 
-export default function StateMachines() {
-  return (
-    <>
-      <PageMeta title="can we create abundance using state machines" />
-
-      <BackLink scrollTarget="thoughts">thoughts</BackLink>
-
-      <article className="section">
-        <h1>can we create abundance using state machines</h1>
-      </article>
-    </>
-  )
+export default function StateMachinesRedirect() {
+  return null
 }
