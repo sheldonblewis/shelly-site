0ce2a2fc7614a6df4f083c0bc1ffa8bdeee92dab
Sheldon Lewis <sheldonblewis@gmail.com>
2026-08-25 01:47:31 -0400

    Add state machines thought page

---
+++ pages/state-machines.js
@@ -0,0 +1,16 @@
+import PageMeta from '@/components/PageMeta'
+import BackLink from '@/components/BackLink'
+
+export default function StateMachines() {
+  return (
+    <>
+      <PageMeta title="can we create abundance using state machines" />
+
+      <BackLink href="/#thoughts">my thoughts</BackLink>
+
+      <article className="section">
+        <h1>can we create abundance using state machines</h1>
+      </article>
+    </>
+  )
+}
