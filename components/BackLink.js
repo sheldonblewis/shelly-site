75cb91dc011f5cb84a7ca54b695a1107a908384c
Sheldon Lewis <sheldonblewis@gmail.com>
2026-08-25 01:54:05 -0400

    Keep article back links on a clean homepage URL

--- components/BackLink.js
+++ components/BackLink.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 
-export default function BackLink({ href, children }) {
+export default function BackLink({ href = '/', scrollTarget, children }) {
+  const rememberScrollTarget = () => {
+    if (scrollTarget) window.sessionStorage.setItem('home-scroll-target', scrollTarget)
+  }
+
   return (
     <div className="back-link">
-      <Link href={href}>
+      <Link href={href} onClick={rememberScrollTarget}>
         ← {children}
       </Link>
     </div>
