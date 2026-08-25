555d30835f542f538d37e1de7b730f46c4687fe5
Sheldon Lewis <sheldonblewis@gmail.com>
2026-08-25 16:36:50 -0400

    Redirect legacy Veena route

--- pages/veena/index.js
+++ pages/veena/index.js
@@ -1,31 +1,12 @@
-import { MDXRemote } from 'next-mdx-remote'
-import PageMeta from '@/components/PageMeta'
-import BackLink from '@/components/BackLink'
-import { getContentByName } from '@/lib/mdx'
-
-export async function getStaticProps() {
-  const veena = await getContentByName('veena')
-  return { props: { veena } }
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/thoughts/veena',
+      permanent: true,
+    },
+  }
 }
 
-export default function Veena({ veena }) {
-  return (
-    <>
-      <PageMeta title="veena" />
-
-      <BackLink scrollTarget="thoughts">thoughts</BackLink>
-
-      <div className="section">
-        <h1>veena</h1>
-        <aside className="retrospective-note">
-          <p>
-            <span>note from aug 25, 2026:</span> here are the scribbling thoughts and ambitions of 21-year-old me. in hindsight, i recognize a continued difficulty in execution, but i appreciate the desire to self-express and share thoughts.
-          </p>
-        </aside>
-        <div className="prose">
-          <MDXRemote {...veena.mdxSource} />
-        </div>
-      </div>
-    </>
-  )
+export default function VeenaRedirect() {
+  return null
 }
